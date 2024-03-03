f0b4dfae3f8d3ed6f1eae27750585731cd18c619
Raj Rasane <rasaneraj04@gmail.com>
2024-03-03 08:57:57 +0530

    added new program

---
+++ 31.pattern3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+    int n,i,j;
+
+    printf("Enter how many rows you want\n");
+    scanf("%d",&n);
+
+    for (i = 1; i <= n ; i++)
+    {
+        for(j = n;j >= i;j--){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+}
