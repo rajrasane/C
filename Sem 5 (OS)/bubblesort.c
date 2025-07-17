486f5227b886e051c3c72b48550627a1527452e6
bcs <bcs@localhost.localdomain>
2025-07-17 10:42:45 +0530

    improved logic

--- Sem 5 (OS)/bubblesort.c
+++ Sem 5 (OS)/bubblesort.c
@@ -8,7 +8,7 @@ void swap(int *a,int *b){
 
 void sort(int *arr,int n){
 	int i,j;
-	for(i=0;i<n-1;i++){
+	for(i=0;i<n;i++){
 		for(j=0;j<n-i-1;j++){
 			if(arr[j]>arr[j+1]){
 				swap(&arr[j],&arr[j+1]);
